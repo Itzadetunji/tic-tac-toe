cad090cd5fe5986f7fa1478061bb6601a9ba67db
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2021-04-18 20:43:47 +0100

    Updated sketch.js

--- sketch.js
+++ sketch.js
@@ -10,7 +10,7 @@ let available = [];
 
 function setup(){
     createCanvas(400, 400);
-    currentPlayer = random(players);
+    currentPlayer = floor(random(players.length));
     for (let j = 0; j < 3; j++) {
         for (let i = 0; i < 3; i++) {
             available.push([i,j]);
