efddf35612970421fbe7351b4c34a1da7ac7b268
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2021-04-19 04:20:37 +0100

    Updated sketch.js

--- sketch.js
+++ sketch.js
@@ -20,7 +20,7 @@ function setup(){
 }
 
 function equals3(a,b,c){
-    return (a == b && b == c && a != ''); 
+    return a == b && b == c && a != ''; 
 }
 
 function checkWinner() {
@@ -35,7 +35,7 @@ function checkWinner() {
 
     //Vertical
     for (let i = 0; i < 3; i++) {
-        if  (equals3(board[0][i],board[0][i],board[0][i])){
+        if  (equals3(board[0][i],board[1][i],board[2][i])){
             winner = board[0][i];
         }      
     }
@@ -72,26 +72,26 @@ function draw(){
     background(255);
     let w = width / 3;
     let h = height /3;
+    strokeWeight(4);
 
-    line(w,0,w,height);
-    line(w*2,0,w*2,height);
-    line(0,h,width,h);
-    line(0,h*2,width,h*2);
+    line(w, 0, w, height);
+    line(w * 2, 0, w * 2, height);
+    line(0, h, width, h);
+    line(0, h * 2, width, h * 2);
 
     for (let j = 0; j < 3; j++) {
         for (let i = 0; i < 3; i++) {
-            let x = w * i + w/2;
-            let y = h * j + h/2;
+            let x = w * i + w / 2;
+            let y = h * j + h / 2;
             let spot = board[i][j];
             textSize(32);
-            strokeWeight(4);
+            let xr = w / 4;
             if  (spot == players[1]) {
                 noFill();
-                ellipse(x,y,w/2);
+                ellipse(x, y, xr*2);
             } else if (spot == players[0]){
-                let xr = w/4;
                 line(x-xr, y-xr, x + xr, y + xr);
-                line(x+xr, y-xr, x-xr, y + xr);
+                line(x+xr, y-xr, x - xr, y + xr);
             }
 
         }
@@ -100,7 +100,13 @@ function draw(){
     let result = checkWinner();
     if(result != null){
         noLoop();
-        createP(result).style('color', '#FFF').style('font-size', '32pt');
+        let resultP = createP('');
+        resultP.style('font-size', '32pt');
+        if (result == 'tie') {
+            resultP.html('Tie');
+        }else{
+            resultP.html(`${result} wins!`);
+        }
     }else{
         nextTurn();
     }
